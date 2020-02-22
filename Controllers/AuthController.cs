c52c351d895ae64d69ae4d68fbc64e4437d49229
Naime Ahmed <naime1947@gmail.com>
2020-02-22 16:04:11 +0600

    Jwt token generated after login

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,8 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using workshopproject.API.Data;
 using workshopproject.API.Dtos;
 using workshopproject.API.Helpers;
@@ -17,13 +24,17 @@ namespace workshopproject.API.Controllers
         private readonly UserManager<User> _userManager;
         private readonly SignInManager<User> _signInManager;
         private readonly IMapper _mapper;
+        private readonly IConfiguration _config;
         
 
-        public AuthController(UserManager<User> userManager, SignInManager<User> signInManager, IMapper mapper)
+        public AuthController(UserManager<User> userManager, 
+        IConfiguration config,
+        SignInManager<User> signInManager, IMapper mapper)
         {
             _userManager = userManager;
             _mapper = mapper;
             _signInManager = signInManager;
+            _config = config;
         }
 
         [HttpPost("Register")]
@@ -39,6 +50,7 @@ namespace workshopproject.API.Controllers
             return BadRequest(result.Errors);
         }
 
+        [HttpPost("Login")]
         public async Task<IActionResult> Login(UserToLoginDto user)
         {
             var userToLogin = await _userManager.FindByNameAsync(user.UserName);
@@ -47,12 +59,34 @@ namespace workshopproject.API.Controllers
                 var appUser = await _userManager.Users.FirstOrDefaultAsync(user=> user.NormalizedUserName == userToLogin.UserName.ToUpper());
                 var userToReturn = _mapper.Map<UserToReturn>(appUser);
                 return Ok(new {
-                    token="",
+                    token=GenerateToken(appUser).Result,
                     user=userToReturn
                 });
             }
 
             return Unauthorized();            
+        }
+
+        private async Task<string> GenerateToken(User user)
+        {
+            var claims = new List<Claim> {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.UserName)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            var tokenDescriptor = new SecurityTokenDescriptor(){
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = credentials
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+
         }
     }
 }
